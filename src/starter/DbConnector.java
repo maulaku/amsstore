fc835148dce383839249ac00e1f7a6baf7a4621c
dwr169 <dwr169@9ad43b18-0c42-11de-b321-f573fdaff03e>
2009-03-09 22:30:29 +0000

    -added setup comments

--- src/starter/DbConnector.java
+++ src/starter/DbConnector.java
@@ -1,36 +1,67 @@
 package starter;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 
 public class DbConnector {
-	
-		public static void main(String args[])
+	public static void main(String args[])
+	{
+		try
 		{
-			try
-			{
-				System.out.println("-start");
-				DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-			    Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:9998:ug", "ora_v6c6", "a83455063");
-			    //"jdbc:oracle:thin:@localhost:9998:ug" 
-			    //"jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1521:ug"
-			    Statement stmt = con.createStatement();
-			    
-			    ResultSet rs = stmt.executeQuery("SELECT * FROM branch");
-			    rs.next();
-			    System.out.print(rs.getString(2));
-			    
-			    System.out.println("\n-end");
-			}
-			catch(SQLException e)
-			{
-				System.out.println("exception caught:" + e.getMessage());
-			}
+			System.out.println("-start");
+			//remember to add classes12.zip to your build path
+			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+		    
+			String username = "ora_v6c6";
+			String password = "a83455063";
+			
+			/*
+			 *In order for this dbUrl to work you must have an XShell connection running.
+			 *Making the XShell connection:
+			 *	Connection category:
+			 *		Protocol: SSH
+			 *		Host: remote.ugrad.cs.ubc.ca
+			 *		Port: 22
+			 *	Tunneling category:
+			 *		-click 'Add...'
+			 *			Type: Local (outgoing)
+			 *			Listen Port: 9998
+			 *			Check 'Accept local connections only'
+			 *			Destination Host: dbhost.ugrad.cs.ubc.ca
+			 *			Destination Port: 1521
+			 */			
+			String dbUrl = "jdbc:oracle:thin:@localhost:9998:ug";
+			
+			Connection con = DriverManager.getConnection(dbUrl, username, password);		    
+		    Statement stmt = con.createStatement();
+		    		    
+		    String sqlText = "SELECT * FROM branch WHERE branch_city='Richmond'";
+		    /*
+		     * if your sqlText is an Insert you must call 'stmt.executeUpdate(sqlText)'
+		     * which returns the number of rows processed
+		    */
+		    ResultSet rs = stmt.executeQuery(sqlText);
+		    
+		    /*
+		     * ResultSet object is like an iterator.  Calling rs.next() will iterate through 
+		     * the tuples.  Then you can call 'rs.getString(x)' where x can be the column number
+		     * or the column title.  There is much more depth to ResultSet but thats as far as 
+		     * I explored.
+		     */
+		    
+		    while(rs.next())
+		    {
+		    	for(int i = 1; i <= 5; i++)
+		    	{
+		    		System.out.print(rs.getString(i) + ", ");
+		    	}
+		    	System.out.print("\n");
+		    }
+		  
+		    System.out.println("\n-end");
+		}
+		catch(SQLException e)
+		{
+			System.out.println("exception caught: " + e.getMessage());
 		}
 	}
-
-
+}
