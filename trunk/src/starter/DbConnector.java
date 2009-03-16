5bc066dd702cca108d1510ba2b96e6b1be2f9c62
dwr169 <dwr169@9ad43b18-0c42-11de-b321-f573fdaff03e>
2009-03-16 22:09:45 +0000

    -initial stable

--- trunk/src/starter/DbConnector.java
+++ trunk/src/starter/DbConnector.java
@@ -1,13 +1,32 @@
 package starter;
+import java.io.*;
 import java.sql.*;
+import java.util.regex.Pattern;
 
 
 public class DbConnector {
+	
+	private static Connection con;
+	
 	public static void main(String args[])
+	{
+			initialize();
+			runScript("src" + File.separator+ "assets" + File.separator + "dropall.txt");
+			/*
+			 * Current Files:
+			 * "src\\assets\\createall.txt"
+			 * "src\\assets\\dropall.txt"
+			 */
+			runScript("src" + File.separator+ "assets" + File.separator+ "createall.txt");
+			update();
+			query();
+			cleanUp();
+	}
+	
+	private static void initialize()
 	{
 		try
 		{
-			System.out.println("-start");
 			//remember to add classes12.zip to your build path
 			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
 		    
@@ -31,22 +50,90 @@ public class DbConnector {
 			 */			
 			String dbUrl = "jdbc:oracle:thin:@localhost:9998:ug";
 			
-			Connection con = DriverManager.getConnection(dbUrl, username, password);		    
-		    Statement stmt = con.createStatement();
-		    		    
-		    String sqlText = "SELECT * FROM branch WHERE branch_city='Richmond'";
-		    /*
-		     * if your sqlText is an Insert you must call 'stmt.executeUpdate(sqlText)'
-		     * which returns the number of rows processed
-		    */
-//		    ResultSet rs = stmt.executeQuery(sqlText);
-		    
+			con = DriverManager.getConnection(dbUrl, username, password);	
+		}
+		catch(SQLException e)
+		{
+			System.out.println("exception caught: " + e.getMessage());
+		}
+	}
+	
+	private static void runScript(String filename)
+	{
+		try 
+		{
+			BufferedReader in = new BufferedReader(new FileReader(filename));
+			String line;
+			String fileText = "";
+			while ((line = in.readLine()) != null) {
+				fileText += line;
+			}
+			in.close();
+			
+			String[] statements = fileText.split(";");
+			Statement stmt = con.createStatement();
+			for(String sqlText : statements){
+				try
+				{
+					stmt.executeUpdate(sqlText);
+					
+				}
+				/*
+				 * this inner try catch is meant to disregard drop table command
+				 * exceptions if the table does not exist
+				 */
+				catch(SQLException e)
+				{
+					System.out.println(e.getMessage());
+				}
+			}
+			stmt.close();
+		} 
+		catch (Exception e) //IOException or SQLException
+		{
+			System.out.println(e.getMessage());
+		}
+
+	}
+	
+	private static void update()
+	{
+		try
+		{
+			Statement stmt = con.createStatement();
+			String sqlText = "INSERT INTO Item VALUES(123, 'itemName1', 'CD', 'rock', 'companyName1', 1980, 20.00)";
+			stmt.executeUpdate(sqlText);
+		}
+		catch(SQLException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	private static void query()
+	{
+		try
+		{
+			Statement stmt = con.createStatement();
+			String sqlText = "SELECT * FROM Item";
+
+			ResultSet rs = stmt.executeQuery(sqlText);
+			
 		    /*
 		     * ResultSet object is like an iterator.  Calling rs.next() will iterate through 
 		     * the tuples.  Then you can call 'rs.getString(x)' where x can be the column number
 		     * or the column title.  There is much more depth to ResultSet but thats as far as 
 		     * I explored.
-		     */
+		     */		    
+		    while(rs.next())
+		    {
+		    	for(int i = 1; i <= 2; i++)
+		    	{
+		    		System.out.print(rs.getString(i) + ", ");
+		    	}
+		    	System.out.print("\n");
+		    }
+		     
 		    
 //		    while(rs.next())
 //		    {
@@ -86,11 +173,23 @@ public class DbConnector {
 //		    while (rs.next())
 //		    	System.out.println(rs.getString("text"));
 		    
-		    System.out.println("\n-end");
+		    //System.out.println("\n-end");
 		}
 		catch(SQLException e)
 		{
-			System.out.println("exception caught: " + e.getMessage());
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	private static void cleanUp()
+	{
+		try
+		{
+			con.close();
+		}
+		catch(SQLException e)
+		{
+			System.out.println(e.getMessage());
 		}
 	}
 }
