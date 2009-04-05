7568a7d24cdab103f49d8f67c3ebde72f903ff47
jeff.h.cheung <jeff.h.cheung@9ad43b18-0c42-11de-b321-f573fdaff03e>
2009-04-05 08:12:05 +0000

    WIP - Grouping results by category

--- trunk/src/ams/ui/manager/DailySalesPanel.java
+++ trunk/src/ams/ui/manager/DailySalesPanel.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.HashMap;
 import java.util.Vector;
 
 import javax.swing.JPanel;
@@ -43,37 +44,108 @@ public class DailySalesPanel extends JPanel
 	{
 		DefaultTableModel model = (DefaultTableModel) table.getModel();
 		
-		Vector<String> columns = Controller.getInstance().getColumnNames("Item");
-		columns.add("upc");
-		columns.add("quantity");
+		Vector<String> columns = new Vector<String>();
+		columns.add("UPC");
+		columns.add("Category");
+		columns.add("Unit Price");
+		columns.add("Units");
+		columns.add("Total Value");
 
 		Vector<Vector<Object>> data = new Vector<Vector<Object>>();
-		if (columns != null)
+		if(columns != null)
 		{	
-			//TODO: Group by category
-			String queryString = "SELECT I.*, PI.upc, PI.quantity FROM Item I, PurchaseItem PI WHERE PI.receiptId IN (SELECT receiptId FROM Purchase WHERE date = current_date)";
-			
 			try
 			{
-				PreparedStatement statement = Controller.getInstance().getConnection().prepareStatement(queryString);
+				//TODO: Change back to current_date
+				PreparedStatement statement = Controller.getInstance().getConnection().prepareStatement("SELECT i.upc, i.category, i.price, pi.quantity FROM Item i, PurchaseItem pi WHERE pi.receiptId IN (SELECT receiptId FROM Purchase WHERE purchasedate = ?) ORDER BY i.category ASC");
+				statement.setDate(1, java.sql.Date.valueOf("2009-04-01"));
 				ResultSet results = statement.executeQuery();
 				
-				// add data into the table
-				while (results.next())
+				//Group results by category
+				HashMap<String, Vector<Vector<Object>>> categories = new HashMap<String, Vector<Vector<Object>>>();
+				while(results.next())
 				{
 					Vector<Object> rowData = new Vector<Object>();
-					for (String columnName : columns)
-						rowData.add(results.getObject(columnName));
-					data.add(rowData);
+
+					rowData.add(results.getObject("upc"));
+					
+					String category = (String) results.getObject("category");
+					rowData.add(category);
+					
+					double price = Double.parseDouble((String) results.getObject("price"));
+					rowData.add(price);
+					
+					double quantity = Double.parseDouble((String) results.getObject("quantity"));
+					rowData.add(quantity);
+					
+					double total = price * quantity;
+					rowData.add(total);
+					
+					//Initialize category if it doesn't already exist in the map
+					if(categories.get(category) == null)
+					{
+						categories.put(category, new Vector<Vector<Object>>());
+					}
+					
+					categories.get(category).add(rowData);
 				}
+				
+				double grandTotalValue = 0.00;
+				int grandTotalUnits = 0;
+				for(Vector<Vector<Object>> category : categories.values())
+				{
+					double totalValue = 0.00;
+					
+					//Add category rows to data set
+					for(Vector<Object> row : category)
+					{
+						totalValue += Double.parseDouble((String) row.get(row.size() - 1));
+						data.add(row);
+					}
+					
+					//Add to grand total
+					grandTotalValue += totalValue;
+					grandTotalUnits += category.size();
+					
+					//Add category's total to data set
+					Vector<Object> categoryTotal = new Vector<Object>();
+					categoryTotal.add(null);
+					categoryTotal.add("Total");
+					categoryTotal.add(null);
+					categoryTotal.add(category.size());
+					
+					categoryTotal.add(totalValue);
+					data.add(categoryTotal);
+				}
+
+				//Add spacer
+				Vector<Object> spacer = new Vector<Object>();
+				spacer.add(null);
+				spacer.add(null);
+				spacer.add(null);
+				spacer.add(null);
+				spacer.add(null);
+				
+				//TODO: Display total daily sales outside of table
+				//Add total daily sales to data set
+				Vector<Object> total = new Vector<Object>();
+				total.add(null);
+				total.add("Total Daily Sales");
+				total.add(null);
+				total.add(grandTotalUnits);
+				total.add(grandTotalValue);
+				
+				data.add(total);
+				
 				statement.close();
 			}
 			catch (Exception e)
 			{
-				
+				System.err.println(e.getMessage());
+				e.printStackTrace();
 			}
-			
 		} 
+		
 		// display it in the table
 		model.setDataVector(data, columns);
 	}
